f5c50a9670b58087935e74b15b6ab27a5279814a
“Ruq23” <daruq20@gmail.com>
2022-08-14 12:38:45 +0100

    fix

--- tcdem/settings.py
+++ tcdem/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'tcdem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tcdemo2',
         'USER': 'postgres',
         'PASSWORD': 'admin',
